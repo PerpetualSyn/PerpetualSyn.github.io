b21626fcbf0b4ce05cbeeac5fc13d61e02c8a6b4
PerpetualSyn <perpetualsyn@gmail.com>
2021-08-17 18:35:37 -0400

    Demo commit 3
    Added Activity report page, got report modal working. Added new css page for activity feed. Got Sidebar and updated filter working for activity feed.

--- index.js
+++ index.js
@@ -7,3 +7,46 @@
 // *************************************************************
 // ****                End Elliott Tests                    ****
 // *************************************************************
+
+
+// let filterCards = ;
+
+let filterCards = (arg) => {
+    let items = document.querySelectorAll('.artistCard');
+    // console.log(typeof items);
+    // for (const element of items) {
+        // console.log(element);
+    //   }
+    for (let el of items) el.style.display = 'none';
+    // for (let el of items) el.classList.add("hidden");;
+    
+
+    console.log("done");
+}
+
+let generateReport = () => {
+    console.log("report");
+
+}
+
+let filterCardsArtist = (arg) => {
+    let items = document.querySelectorAll('.clientCard');
+    // console.log(typeof items);
+    // for (const element of items) {
+        // console.log(element);
+    //   }
+    for (let el of items) el.style.display = 'none';
+    // for (let el of items) el.classList.add("hidden");;
+    
+
+    console.log("done");
+}
+
+
+
+
+// row-cols-1 row-cols-md-3 g-4
+
+
+
+// for (let el of document.querySelectorAll('.artistCard')) el.style.visibility = 'hidden';
